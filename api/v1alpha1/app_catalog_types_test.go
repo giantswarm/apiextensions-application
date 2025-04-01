e083a2cd18f207430159c1e323506b5d0c514143
Marian Steinbach <marian@giantswarm.io>
2025-04-01 10:27:00 +0200

    Adapt for golangci-lint v2 (#172)
    * Mitigate linter complaints
    
    * Bump dependencies

--- api/v1alpha1/app_catalog_types_test.go
+++ api/v1alpha1/app_catalog_types_test.go
@@ -48,7 +48,7 @@ func Test_GenerateAppCatalogYAML(t *testing.T) {
 					t.Fatal(err)
 				}
 			}
-			goldenFile, err := os.ReadFile(path)
+			goldenFile, err := os.ReadFile(path) //nolint:gosec
 			if err != nil {
 				t.Fatal(err)
 			}
